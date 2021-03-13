ecede80260916a9dfcd95c797aee842bf0c20fb9
Ростислав Абалов <gasick@localhost.localdomain>
2021-03-13 13:52:57 +0300

    small fixes

---
+++ README.md
@@ -0,0 +1,11 @@
+Start docker-compose
+```
+docker-compose up -d
+```
+
+run script
+```
+./firstrun.sh
+```
+
+Here you got: localhost:8080/guacamole
--- firstrun.sh
+++ firstrun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > initdb.sql
-docker-compose exec guacpsql dropdb -U guac_user_db guac_db
-docker-compose exec guacpsql createdb -U guac_user_db guac_db
-docker-compose exec -T guacpsql psql -U guac_user_db guac_db < initdb.sql
+docker-compose exec guacpsql dropdb -U guacamole_user guacamole_db
+docker-compose exec guacpsql createdb -U guacamole_user guacamole_db
+docker-compose exec -T guacpsql psql -U guacamole_user guacamole_db < initdb.sql
 
